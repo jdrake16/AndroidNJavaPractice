3cd6a8c493bcee04d9437ecd2fdaa94e87d94d84
jdrake16 <stovetops111@gmail.com>
2020-09-16 12:45:54 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/JelloWorld.java
@@ -0,0 +1,48 @@
+package edu.cnm.deepdive;
+
+public class JelloWorld {
+
+  public static void main(String[] args) {
+
+//    int age = 29;
+//
+//    int ageInADecade = age + 10;
+//
+//    System.out.println(ageInADecade);
+
+
+//    boolean isLifeMeaningless = true;
+//
+//    System.out.println(isLifeMeaningless);
+
+//    String myName = "Jason";
+//
+//    int myAge = 29;
+//
+//    System.out.println("My name is " + myName + " (" +myAge + ") " + " He was # 1");
+
+//    double a = 1.6;
+//
+//    double b = 6.9;
+//
+//    System.out.println(a + b - 8.5);
+
+//    String gString = "the g string on a bass duh";
+//
+//    System.out.println(gString.length());
+
+    String firstName = "ever";
+
+    String lastName = "greatest";
+
+    int forever = firstName.length() + lastName.length();
+
+    System.out.println("total characters = " + forever);
+  }
+
+
+
+
+
+
+}
