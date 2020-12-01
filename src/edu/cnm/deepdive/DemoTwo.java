aa7bb701dd80e15ae207e8c76b55c3b9a35c9c6c
jdrake16 <stovetops111@gmail.com>
2020-12-01 14:56:57 -0700

    operators practice

---
+++ src/edu/cnm/deepdive/DemoTwo.java
@@ -0,0 +1,26 @@
+package edu.cnm.deepdive;
+
+public class DemoTwo {
+
+  static int lives = 3;
+  static int coins = 90;
+
+  public static void main(String[] args) {
+
+    while (lives > 0) {  // <-- loops while this condition is true ("while" checks condition and then executes code)
+      System.out.println("lives= " + lives);
+      lives --; // same as lives = lives - 1;
+    }
+
+    lives = 3;
+
+    do {    // "do while" executes code and then checks condition
+      coins += 20;
+      lives --;
+    } while (lives > 0); // "do while" requires a semicolon "while" does not
+
+    System.out.println("coins= " + coins + " lives= " + lives);
+
+  }
+
+}
