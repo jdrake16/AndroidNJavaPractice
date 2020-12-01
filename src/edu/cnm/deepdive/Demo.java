da31003534900ef0d17b549b3b16451e56881d4b
jdrake16 <stovetops111@gmail.com>
2020-12-01 14:34:45 -0700

    operators practice

--- src/edu/cnm/deepdive/Demo.java
+++ src/edu/cnm/deepdive/Demo.java
@@ -2,29 +2,38 @@ package edu.cnm.deepdive;
 
 public class Demo {
 
-  static int lives = 3;
-  static int score;
-  static int coinCount = 90;
-  static boolean gameOver;
+  static int lives = 3; //mutable/changeable
+  static int score; // 0 by default
+  static int coins = 40;
+
 
   public static void main(String[] args) {
+
+    System.out.println("score= " + score);
+
+    //score = score + 10; or
     score += 10;
-    coinCount = 75;
-    lives = 0;
 
-    if (lives == 0) {
-      gameOver = true;
-    }
+    System.out.println("score= " + score + " lives= " + lives);
 
-    if (coinCount > 100) {
-      System.out.println("gold medal");
-    } else if (coinCount> 80) {
-      System.out.println("silver medal");
-    } else if (coinCount > 70) {
-      System.out.println("bronze medal");
-    } else {
-      System.out.println("no medal");
-    }
+    //lives = lives - 1; or
+    //lives -= 1; or
+    lives --;
+
+    System.out.println("score= " + score + " lives= " + lives);
+
+    //score = score * 2; or
+    score *= 2;
+
+    System.out.println("score= " + score + " lives= " + lives);
+
+    //coins = coins + 40;
+    coins += 40;
+
+    System.out.println("coins= " + coins);
+
+
+
   }
 
 }
